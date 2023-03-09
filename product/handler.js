b0ec046bd64d6c04fbcdbd1b107cfe09cc834197
camarenaepam <jonatan.camarena@epam.com>
2023-03-08 19:01:24 -0700

    task-4

--- product/handler.js
+++ product/handler.js
@@ -1,42 +1,13 @@
 'use strict';
+const { DynamoDB } = require("aws-sdk");
+const { Converter: { unmarshall }  } = DynamoDB;
 
-const results = [
-  {
-    id: 1,
-    title: 'product red',
-    price: 100,
-    image: 'https://cdn.homedepot.com.mx/productos/150169/150169-z.jpg',
-    count: 1
-  },
-  {
-    id: 2,
-    title: 'product blue',
-    price: 300,
-    image: 'https://m.media-amazon.com/images/I/51Eg49ZtnlL._AC_SL1001_.jpg',
-    count: 2
-  }, 
-  {
-    id: 3,
-    title: 'product green',
-    price: 300,
-    image: 'https://m.media-amazon.com/images/I/71oobehHW-L._AC_SL1500_.jpg',
-    count: 3
-  },
-   {
-    id: 4,
-    title: 'product yellow',
-    price: 400,
-    image: 'https://m.media-amazon.com/images/I/7163ck4COpL._AC_SL1500_.jpg',
-    count: 4
-  }, 
-  {
-    id: 5,
-    title: 'product purple',
-    price: 500,
-    image: 'https://m.media-amazon.com/images/I/81mXeHhatZL._AC_SL1500_.jpg',
-    count: 5
-  },
-]
+
+const headers =  {
+  "Access-Control-Allow-Headers" : "Content-Type",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+};
 
 module.exports.hello = async (event) => {
   return {
@@ -50,28 +21,88 @@ module.exports.hello = async (event) => {
 };
 
 module.exports.getProductList = async (event) => {
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Headers" : "Content-Type",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-    },
-    body: JSON.stringify({ results })
-  };
+  try {
+    const db = new DynamoDB.DocumentClient();
+    const tableProducts = 'tableNameProducts';
+    const tableStock = 'tableNameStock';
+    
+    const results = await db.scan({ TableName: tableProducts }).promise();
+    const stock = await db.scan({ TableName: tableStock }).promise();
+    
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ results, stock })
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ err })
+    }
+  }
+};
+
+module.exports.createProduct = async (event) => {
+  const db = new DynamoDB.DocumentClient();
+  try {
+    const TableName = 'tableNameProducts';
+    
+    const params = {
+      TableName,
+      Item: JSON.parse(event.body)
+    }
+
+    var result = await db.put(params).promise();
+    
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ result })
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ err })
+    }
+  }
 };
 
 module.exports.getProductById = async (event) => {
-  const result = results.find(item => item.id === parseInt(event.pathParameters.productId))
-  if (!result) {
+  try {
+    const db = new DynamoDB();
+
+    const TableName = 'tableNameProducts';
+    const productID = event.pathParameters.productId;
+
+    var params = {
+      Key: {
+       "id": {"N": productID}
+      }, 
+      TableName
+    };
+
+    var result = await db.getItem(params).promise();
+
+    if (!result) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: "product doesn't exist" })
+      }
+    }
+
     return {
-      statusCode: 404,
-      body: JSON.stringify({ message: "product doesn't exist" })
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ result: unmarshall(result.Item)})
+    };
+  } catch (err) {
+    console.log('err:', err);
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ err })
     }
   }
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ result })
-  };
-
 };
